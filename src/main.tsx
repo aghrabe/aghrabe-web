a7afb95977d5cd08c577011b727ad76143f9b3e6
arashjp8 <arashjafarpour723@gmail.com>
2025-03-13 09:19:18 +0330

    [fix] - forgot to remove pocketbase provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import AuthProvider from "./context/AuthContext.tsx";
-import { PocketBaseProvider } from "./context/PocketBaseContext.tsx";
-import PocketBase from "pocketbase";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <PocketBaseProvider value={new PocketBase("http://localhost:8090")}>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </PocketBaseProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </StrictMode>,
 );
