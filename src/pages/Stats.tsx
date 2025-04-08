bf4f69c7110fed3fecf15aca82bff2c1b1f9bccf
arashjp8 <arashjafarpour723@gmail.com>
2025-04-08 13:14:14 +0330

    [feat] - dynamic theme for chart

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -11,9 +11,11 @@ import {
     Title,
     Tooltip,
 } from "chart.js";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Line } from "react-chartjs-2";
 import Header from "../components/Header";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { useTheme } from "../context/ThemeContext";
 import useGames from "../hooks/useGames";
 
 ChartJS.register(
@@ -27,82 +29,6 @@ ChartJS.register(
     Tooltip,
     Legend,
 );
-
-const mockData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "Played Time",
-            data: [65, 59, 80, 81, 56, 20, 40],
-            fill: false,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            pointStyle: "circle",
-            pointRadius: 10,
-            pointHoverRadius: 15,
-        },
-        {
-            label: "Mood Before",
-            data: [81, 56, 33, 65, 59, 80, 40],
-            fill: false,
-            backgroundColor: "rgba(192,75,192,0.4)",
-            borderColor: "rgba(192,75,192,1)",
-            pointStyle: "circle",
-            pointRadius: 10,
-            pointHoverRadius: 15,
-        },
-        {
-            label: "Mood After",
-            data: [55, 81, 40, 10, 59, 65, 80],
-            fill: false,
-            backgroundColor: "rgba(192,192,75,0.4)",
-            borderColor: "rgba(192,192,75,1)",
-            pointStyle: "circle",
-            pointRadius: 10,
-            pointHoverRadius: 15,
-        },
-    ],
-};
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-        legend: {
-            display: false,
-            position: "bottom",
-        },
-        title: {
-            display: false,
-            text: "Play Time",
-        },
-    },
-} as ChartOptions<"line">;
-
-//const totalPlayed = mockData.datasets[0].data.reduce((a, b) => a + b, 0);
-//const totalMoodBefore = mockData.datasets[1].data.reduce((a, b) => a + b, 0);
-//const totalMoodAfter = mockData.datasets[2].data.reduce((a, b) => a + b, 0);
-
-//const doughnutData = {
-//    labels: ["Played Time", "Mood Before", "Mood After"],
-//    datasets: [
-//        {
-//            label: "Total Summary",
-//            data: [totalPlayed, totalMoodBefore, totalMoodAfter],
-//            backgroundColor: [
-//                "rgba(75,192,192,0.6)",
-//                "rgba(192,75,192,0.6)",
-//                "rgba(192,192,75,0.6)",
-//            ],
-//            borderColor: [
-//                "rgba(75,192,192,1)",
-//                "rgba(192,75,192,1)",
-//                "rgba(192,192,75,1)",
-//            ],
-//            borderWidth: 1,
-//        },
-//    ],
-//};
 
 interface StatCardProps {
     title: string;
@@ -126,6 +52,151 @@ function StatCard({ title, value }: StatCardProps) {
 
 export default function Stats() {
     const { gamesState } = useGames();
+    const { globalTheme } = useTheme();
+
+    const getThemeColors = useCallback(() => {
+        let themeColors: {
+            primary: string;
+            error: string;
+            success: string;
+            info: string;
+        };
+
+        switch (globalTheme) {
+            case "Dark":
+                themeColors = {
+                    primary: "#3b82f6",
+                    error: "#ef4444",
+                    success: "#10b981",
+                    info: "#06b6d4",
+                };
+                break;
+            case "Catppuccin":
+                themeColors = {
+                    primary: "#89b4fa",
+                    error: "#f38ba8",
+                    success: "#a6e3a1",
+                    info: "#89dceb",
+                };
+                break;
+            case "Tokyonight":
+                themeColors = {
+                    primary: "#7aa2f7",
+                    error: "#f7768e",
+                    success: "#9ece6a",
+                    info: "#7dcfff",
+                };
+                break;
+
+            default:
+                themeColors = {
+                    primary: "#2563eb",
+                    error: "#dc2626",
+                    success: "#059669",
+                    info: "#0891b2",
+                };
+                break;
+        }
+
+        return themeColors;
+    }, [globalTheme]);
+
+    const [themeColors, setThemeColors] = useState(getThemeColors());
+
+    useEffect(() => {
+        setThemeColors(getThemeColors());
+    }, [globalTheme, getThemeColors]);
+
+    useEffect(() => {
+        console.log(themeColors);
+    }, [themeColors]);
+
+    const mockData = useMemo(
+        () => ({
+            labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+            ],
+            datasets: [
+                {
+                    label: "Played Time",
+                    data: [65, 59, 80, 81, 56, 20, 40],
+                    fill: false,
+                    backgroundColor: `${themeColors.primary}66`,
+                    borderColor: themeColors.primary,
+                    pointStyle: "circle",
+                    pointRadius: 10,
+                    pointHoverRadius: 15,
+                },
+                {
+                    label: "Mood Before",
+                    data: [81, 56, 33, 65, 59, 80, 40],
+                    fill: false,
+                    backgroundColor: `${themeColors.error}66`,
+                    borderColor: themeColors.error,
+                    pointStyle: "circle",
+                    pointRadius: 10,
+                    pointHoverRadius: 15,
+                },
+                {
+                    label: "Mood After",
+                    data: [55, 81, 40, 10, 59, 65, 80],
+                    fill: false,
+                    backgroundColor: `${themeColors.info}66`,
+                    borderColor: themeColors.info,
+                    pointStyle: "circle",
+                    pointRadius: 10,
+                    pointHoverRadius: 15,
+                },
+            ],
+        }),
+        [themeColors],
+    );
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+                position: "bottom",
+            },
+            title: {
+                display: false,
+                text: "Play Time",
+            },
+        },
+    } as ChartOptions<"line">;
+
+    //const totalPlayed = mockData.datasets[0].data.reduce((a, b) => a + b, 0);
+    //const totalMoodBefore = mockData.datasets[1].data.reduce((a, b) => a + b, 0);
+    //const totalMoodAfter = mockData.datasets[2].data.reduce((a, b) => a + b, 0);
+
+    //const doughnutData = {
+    //    labels: ["Played Time", "Mood Before", "Mood After"],
+    //    datasets: [
+    //        {
+    //            label: "Total Summary",
+    //            data: [totalPlayed, totalMoodBefore, totalMoodAfter],
+    //            backgroundColor: [
+    //                "rgba(75,192,192,0.6)",
+    //                "rgba(192,75,192,0.6)",
+    //                "rgba(192,192,75,0.6)",
+    //            ],
+    //            borderColor: [
+    //                "rgba(75,192,192,1)",
+    //                "rgba(192,75,192,1)",
+    //                "rgba(192,192,75,1)",
+    //            ],
+    //            borderWidth: 1,
+    //        },
+    //    ],
+    //};
 
     if (gamesState.isLoading) {
         return (
